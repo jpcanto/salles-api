24757178ccb83bd222bf2ec538b596cb6deddbfa
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-03-08 13:44:12 -0300

    feat: create product repository

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,17 @@
+import { Repository } from 'typeorm';
+import Product from '../entities/Product';
+
+export class ProductRepository extends Repository<Product> {
+
+  public async findByName(name: string): Promise<Product | null> {
+    const product = this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
+  }
+  
+
+}
