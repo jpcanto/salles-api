06cb58535626e116b2e4ff14314d0e9c45725dca
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-03-08 13:06:28 -0300

    feat: create product entity

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+}
+
+export default Product;
