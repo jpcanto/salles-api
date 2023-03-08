e3b6a5fb6c6a411a4f9fc7aa08af2a0f4bd25b6e
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-03-08 20:19:33 -0300

    feat: add request schema middlewares

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/ProductsController';
+import { productIdMiddleware, productMiddleware } from './middlewareSchemas';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.index);
-productsRouter.get('/:id', productsController.show);
-productsRouter.post('/', productsController.create);
-productsRouter.put('/:id', productsController.update);
-productsRouter.delete('/:id', productsController.delete);
+productsRouter.get(
+  '/:id',
+  productIdMiddleware.execute,
+  productsController.show
+);
+productsRouter.post('/', productMiddleware.execute, productsController.create);
+productsRouter.put(
+  '/:id',
+  productIdMiddleware.execute,
+  productMiddleware.execute,
+  productsController.update
+);
+productsRouter.delete(
+  '/:id',
+  productIdMiddleware.execute,
+  productsController.delete
+);
 
 export default productsRouter;
---
+++ src/shared/http/middlewares/RequestValidationMiddleware.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import { Schema } from 'joi';
+import { ParsedQs } from 'qs';
+
+type RequestStData = {
+  body: Record<string, any>;
+  query: ParsedQs;
+  params: Record<string, string>;
+  headers: Record<string, string>;
+};
+
+export default class RequestValidationMiddleware {
+  private schema: Schema;
+  private strategy: keyof RequestStData;
+
+  constructor(schema: Schema, strategy: keyof RequestStData) {
+    this.schema = schema;
+    this.strategy = strategy;
+  }
+
+  execute = (req: Request, res: Response, next: NextFunction) => {
+    const { error } = this.schema.validate(req[this.strategy]);
+
+    console.log("==================> ERROR:")
+
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
+
+    next();
+  }
+}
