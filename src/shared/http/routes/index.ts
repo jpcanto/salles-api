242b937285a73666346dcb0f4acaf8e811b8eb15
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-03-08 15:34:45 -0300

    feat: add product route

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
+
+routes.use('/products', productsRouter);
 
 routes.get('/health', (request, response) => {
   return response.json({
