995548d414cb0005eaeba45d7197cf6e1e355b6a
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-03-24 11:24:59 -0300

    feat: add user update service

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,39 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import { UsersRepository } from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+class UpdateUserService {
+  public async execute({ id, name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const isUserExists = await usersRepository.findByEmail(email);
+
+    const user = await usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('user not found.');
+    }
+
+    if (isUserExists && email !== user.email) {
+      throw new AppError('There is already exists one user with this email');
+    }
+
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    user.avatar = "";
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserService;
