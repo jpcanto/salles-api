cc2bd8af9bcd0eac8fe3ac45c7a09556a8240bde
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-04-06 14:18:11 -0300

    fix: error log position

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -13,8 +13,9 @@ app.use(express.json());
 app.use(routes);
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
+    console.log(`ERROR ========> ${JSON.stringify(error)}`);
+
     if (error instanceof AppError) {
-      console.log(`ERROR ========> ${JSON.stringify(error)}`);
       return response.status(error.statusCode).json({
         status: 'error',
         message: error.message,
