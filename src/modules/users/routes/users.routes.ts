b1c0c1cda1311efc50fa8a6d8f59c987c76c263a
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-04-06 16:34:08 -0300

    feat: add auth middleware

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,18 +1,35 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import isAuthenticated from '../middlewares/isAuthenticated';
 import { usersIdMiddleware, usersBodyMiddleware } from './router.middleware';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
-usersRouter.get('/', usersController.index);
+usersRouter.get('/', isAuthenticated, usersController.index);
 
-usersRouter.get('/:id', usersIdMiddleware.execute, usersController.show);
+usersRouter.get(
+  '/:id',
+  isAuthenticated,
+  usersIdMiddleware.execute,
+  usersController.show
+);
 
 usersRouter.post('/', usersBodyMiddleware.execute, usersController.create);
 
-usersRouter.put('/:id', usersIdMiddleware.execute, usersBodyMiddleware.execute, usersController.update);
+usersRouter.put(
+  '/:id',
+  isAuthenticated,
+  usersIdMiddleware.execute,
+  usersBodyMiddleware.execute,
+  usersController.update
+);
 
-usersRouter.delete('/:id', usersIdMiddleware.execute, usersController.delete);
+usersRouter.delete(
+  '/:id',
+  isAuthenticated,
+  usersIdMiddleware.execute,
+  usersController.delete
+);
 
 export default usersRouter;
