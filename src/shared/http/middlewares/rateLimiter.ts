6e487bfcceafc8f6c79bb64106ebf3e8dbca9979
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-04-07 01:22:13 -0300

    fix: drop unused options

--- src/shared/http/middlewares/rateLimiter.ts
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -4,27 +4,21 @@ import { RateLimiterRedis } from 'rate-limiter-flexible';
 import AppError from '@shared/errors/AppError';
 import cacheConfig from '@config/cache';
 
-const redisOptions = {
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS || undefined,
-};
-
-const redisClient = new Redis(cacheConfig.config.redis);
-
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'ratelimit',
-  points: 5,
-  duration: 1,
-});
-
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
   try {
+    const redisClient = new Redis(cacheConfig.config.redis);
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 5,
+      duration: 1,
+    });
+
     await limiter.consume(request.ip);
 
     return next();
