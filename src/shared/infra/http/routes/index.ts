0cce36870c50e65fc1afea62405ce3dedc2f24a9
@joao.pedro.lopes <joao.pedro.lopes@g.globo>
2023-04-07 09:53:01 -0300

    fix: adjust paths

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
-routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 
